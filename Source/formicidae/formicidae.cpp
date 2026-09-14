bf6f7ac9ba5d36d06cda662becc8e96bd2c879d1
Auroy <146713957+ChifuyuOnWish@users.noreply.github.com>
2026-09-14 19:16:06 +0200

    docs(world): spatial representation hash or linear
    * chore(documentation): renamed doc to follow naming convention
    
    * docs(world): added doc over spatial representation of world

---
+++ Source/formicidae/formicidae.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "formicidae.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, formicidae, "formicidae" );
